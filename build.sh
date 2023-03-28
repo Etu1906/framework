a95d28160d1b02b1a80e9b900f8a91b4a3bcfe6d
Etu1906 <118957897+Etu1906@users.noreply.github.com>
2023-03-28 10:06:26 +0300

    modification copie du jar

--- build.sh
+++ build.sh
@@ -9,5 +9,6 @@ cp fw.jar  ../../test-framework/WEB-INF/lib/;
 cd /home/ralph/ralph/apache-tomcat-8.5.82/webapps/framework3/test-framework/src;
 javac -d ../WEB-INF/classes *.java;
 cd ../../test-framework;
-jar -cf /home/ralph/ralph/apache-tomcat-8.5.82/webapps/framework4.war .;
+jar -cf framework4.war .;
+cp  framework4.war /home/ralph/ralph/apache-tomcat-8.5.82/webapps;
 cd /home/ralph/ralph/apache-tomcat-8.5.82/webapps/framework3;
